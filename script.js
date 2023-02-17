235afd238aac16749f1e55c00c22d89fcd53ad55
joelstoppa <joel.stoppa@gmail.com>
2023-02-17 22:44:57 +0100

    Add mouseOver functionality

--- script.js
+++ script.js
@@ -3,10 +3,16 @@ const container = document.querySelector('.container')
 function makeGrid(rowNum) {
     for (let i = 0; i < rowNum; i++) {
         const row = container.appendChild(document.createElement('div'));
+        row.className = 'row'
         for (let j = 0; j < rowNum; j++) {
             const box = document.createElement('div');
-            box.className = 'box'
+            box.addEventListener('mouseover', () => {
+                box.style.backgroundColor = 'black';
+            })
+            box.className = 'box';
             row.appendChild(box);
         }
     }
 }
+makeGrid(16)
+
