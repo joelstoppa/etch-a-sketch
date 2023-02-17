be1e81304fa0ab86beb6317b36704942f3714630
joelstoppa <joel.stoppa@gmail.com>
2023-02-17 23:37:02 +0100

    Use grid instead of flex

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
-const container = document.querySelector('.container')
 
 function makeGrid(rowNum) {
-    for (let i = 0; i < rowNum; i++) {
-        const row = container.appendChild(document.createElement('div'));
-        row.className = 'row'
-        for (let j = 0; j < rowNum; j++) {
-            const box = document.createElement('div');
-            box.addEventListener('mouseover', () => {
+    let board = document.querySelector('.board')
+    board.style.gridTemplateColumns = `repeat(${rowNum}, 1fr`
+    board.style.gridTemplateRows = `repeat(${rowNum}), 1fr`
+    
+    let gridSize = rowNum * rowNum
+    for (let i = 0; i < gridSize; i++) {
+        const box = document.createElement('div');
+        board.insertAdjacentElement('beforeend', box)
+        box.addEventListener('mouseover', () => {
                 box.style.backgroundColor = 'black';
             })
-            box.className = 'box';
-            row.appendChild(box);
+            
         }
     }
-}
+
 makeGrid(16)
 
+function changeSize(input) {
+    if (input >= 2 && input <= 100 ){
+        makeGrid(input)
+    }
+    else {
+        alert('Number is too not supported')
+    }
+
+}
+    
