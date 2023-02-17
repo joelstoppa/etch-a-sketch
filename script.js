c373af6a731c53468d7d392ecf6ffd7ac58213f9
joelstoppa <joel.stoppa@gmail.com>
2023-02-18 00:17:12 +0100

    Add colorSquare, changeColor and clearBoard functions

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
+let color = 'black';
 
 function makeGrid(rowNum) {
-    let board = document.querySelector('.board')
-    board.style.gridTemplateColumns = `repeat(${rowNum}, 1fr`
-    board.style.gridTemplateRows = `repeat(${rowNum}), 1fr`
+    let board = document.querySelector('.board');
+    let boxes = document.querySelectorAll('div');
+    //boxes.forEach((div) => div.remove()); //this clears the board when we changeSize
+    board.style.gridTemplateColumns = `repeat(${rowNum}, 1fr`;
+    board.style.gridTemplateRows = `repeat(${rowNum}), 1fr`;
     
-    let gridSize = rowNum * rowNum
+    let gridSize = rowNum * rowNum;
     for (let i = 0; i < gridSize; i++) {
-        const box = document.createElement('div');
-        board.insertAdjacentElement('beforeend', box)
-        box.addEventListener('mouseover', () => {
-                box.style.backgroundColor = 'black';
-            })
-            
+        let box = document.createElement('div');
+        box.addEventListener('mouseover', colorSquare)
+        board.insertAdjacentElement('beforeend', box);
+        
         }
     }
-
-makeGrid(16)
+    
+    makeGrid(16)
 
 function changeSize(input) {
     if (input >= 2 && input <= 100 ){
@@ -24,6 +25,23 @@ function changeSize(input) {
     else {
         alert('Number is too not supported')
     }
+    
+}
 
+function colorSquare(){
+    if (color === 'random'){
+        this.style.backgroundColor= `hsl(${Math.random() * 360}, 100%, 50%)`
+    }
+    this.style.backgroundColor = color;
+}
+
+function changeColor(choice) {
+    color = choice
+}
+
+function clearBoard(){
+    let board = document.querySelector('.board');
+    let box = document.querySelectorAll('div');
+    box.forEach((div) => div.style.backgroundColor = 'white'); 
 }
     
